0b722bf51b2ccb0a52046ea48a85790d4a217694
bachnguyen.2504 <108047626+BachNguyenn@users.noreply.github.com>
2025-05-09 07:43:11 +0000

    class

---
+++ src/class/student.java
@@ -0,0 +1,90 @@
+public class student {
+    private String name;
+    private int dateOfBirth;
+    private String gender;
+    private String studentId;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String IdentityCard;
+    private String roomNumber;
+    public student(String name, int dateOfBirth, String gender, String studentId, String address, String phoneNumber, String email, String IdentityCard, String roomNumber) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.studentId = studentId;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.IdentityCard = IdentityCard;
+        this.roomNumber = roomNumber;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(int dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getIdentityCard() {
+        return IdentityCard;
+    }
+    public void setIdentityCard(String identityCard) {
+        IdentityCard = identityCard;
+    }
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    @Override 
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", gender='" + gender + '\'' +
+                ", studentId='" + studentId + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                "IdentityCard='" + IdentityCard + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                '}';
+    }
+}  
