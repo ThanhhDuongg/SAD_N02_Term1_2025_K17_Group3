c1df70a55a31a0fce3cf704d8c095a2b659278b2
Nora-LeDuong <leduyduong.2004@gmail.com>
2025-05-23 15:23:36 +0700

    Update studentTest.java

--- src/Test/studentTest.java
+++ src/Test/studentTest.java
@@ -1,1 +1,40 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManager {
+    private List<Student> studentList = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        studentList.add(student);
+    }
+
+    public Student getStudentById(String studentId) {
+        for (Student s : studentList) {
+            if (s.getStudentId().equals(studentId)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentList;
+    }
+  
+    public boolean updateStudent(String studentId, Student updatedStudent) {
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getStudentId().equals(studentId)) {
+                studentList.set(i, updatedStudent);
+                return true;
+            }
+        }
+        return false;
+    }
+  
+    public boolean deleteStudent(String studentId) {
+        return studentList.removeIf(s -> s.getStudentId().equals(studentId));
+    }
+}
 
