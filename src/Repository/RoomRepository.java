95d953ee8e8fc2649bfe7e9e84cbbcdf79208479
Nora-LeDuong <leduyduong.2004@gmail.com>
2025-06-23 21:40:12 +0700

    Update RoomRepository.java

--- src/Repository/RoomRepository.java
+++ src/Repository/RoomRepository.java
@@ -1,12 +1,14 @@
-package Repository;
+package Dorm.repository;
 
-import com.example.dorm.model.Room;
+import Dorm.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     List<Room> findByNumberContainingIgnoreCaseOrTypeContainingIgnoreCase(String number, String type);
+    Page<Room> findByNumberContainingIgnoreCaseOrTypeContainingIgnoreCase(String number, String type, Pageable pageable);
 }
