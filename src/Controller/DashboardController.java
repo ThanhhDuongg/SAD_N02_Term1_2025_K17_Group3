497aa26c27b771036e72c067f27d0ab2370afbed
Nguyễn Thành Dương <duong.nguyenthanh205@gmail.com>
2025-06-24 13:19:50 +0700

    Create DashboardController.java

---
+++ src/Controller/DashboardController.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import Service.StudentService;
+import Service.RoomService;
+import Service.ContractService;
+import Service.FeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
