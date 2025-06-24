f1856d244e6e731e79b7bf6ff5d2546e3003b580
Nguyễn Thành Dương <duong.nguyenthanh205@gmail.com>
2025-06-24 13:20:19 +0700

    Update DashboardController.java

--- src/Controller/DashboardController.java
+++ src/Controller/DashboardController.java
@@ -8,3 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private RoomService roomService;
+    @Autowired
+    private ContractService contractService;
+    @Autowired
+    private FeeService feeService;
