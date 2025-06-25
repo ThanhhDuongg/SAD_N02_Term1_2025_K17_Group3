410f4dc3bb2ff2b9d27855fdd920ef0430871312
Nora-LeDuong <leduyduong.2004@gmail.com>
2025-06-25 09:16:19 +0000

    update RoomRepository.java

--- src/main/java/com/example/dorm/repository/RoomRepository.java
+++ src/main/java/com/example/dorm/repository/RoomRepository.java
@@ -1,6 +1,6 @@
 package com.example.dorm.repository;
 
-import Dorm.model.Room;
+import com.example.dorm.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
